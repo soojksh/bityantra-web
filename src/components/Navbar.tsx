244fc67197d6d600ce4e860116537831168d626c
sooj <soojksh@gmail.com>
2026-02-22 21:28:40 +0545

    navbar file system updation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../index.css";
+import "./navbar.css";
 
 export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
